db1d1c956406a306582744a688bc2f49641376b0
Gary Zhu <dinner3000@hotmail.com>
2017-12-17 23:57:14 +0800

    Add Bean Validation 1.1（JSR-349）examples

--- src/main/java/com/dinner3000/springmvc/controller/DefaultController.java
+++ src/main/java/com/dinner3000/springmvc/controller/DefaultController.java
@@ -1,12 +1,18 @@
 package com.dinner3000.springmvc.controller;
 
 import com.dinner3000.service.DefaultService;
+import com.dinner3000.springmvc.model.DefaultForm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/")
@@ -35,5 +41,12 @@ public class DefaultController {
         return defaultService.getInfo();
     }
 
+    @RequestMapping(value = "submitForm")
+    public @ResponseBody Object submitForm(@Valid DefaultForm form, BindingResult result){
+        if(result.hasErrors()) {
+            return result;
+        }
+        return "success";
+    }
 
 }
