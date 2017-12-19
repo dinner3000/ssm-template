2cb8fc0ed50ed6efcdae7c1f8de343773d0c2ce2
dinner3000 <dinner3000@hotmail.com>
2017-12-19 13:59:56 +0800

    Change to @ControllerAdvice exception handlers, refine log messages, refine aop/interceptor examples
    # Conflicts:
    #	src/main/resources/spring.xml
    Resolved as using master version

--- src/main/java/com/dinner3000/springmvc/advice/MyResponseBodyAdvice.java
+++ src/main/java/com/dinner3000/springmvc/advice/MyResponseBodyAdvice.java
@@ -1,21 +1,27 @@
 package com.dinner3000.springmvc.advice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
-//@ControllerAdvice
+@ControllerAdvice({"com.dinner3000.springmvc.controller"})
 public class MyResponseBodyAdvice implements ResponseBodyAdvice {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     public boolean supports(MethodParameter methodParameter, Class aClass) {
-        System.out.println("###JsonResponseBodyAdvice - supports()");
+        logger.debug("###MyResponseBodyAdvice - supports()");
         return true;
     }
 
     public Object beforeBodyWrite(Object o, MethodParameter methodParameter, MediaType mediaType, Class aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
-        System.out.println("###JsonResponseBodyAdvice - beforeBodyWrite()");
+        logger.debug("###MyResponseBodyAdvice - beforeBodyWrite()");
         HttpHeaders headers = serverHttpRequest.getHeaders();
         return o;
     }
